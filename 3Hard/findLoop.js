8f77ff3c7f9a13266f1847d85ad3152aabbc3cf0
James Fowler <josiah.james.fowler@gmail.com>
2021-03-22 18:42:24 -0400

    updatedFindLoopLogic

--- 3Hard/findLoop.js
+++ 3Hard/findLoop.js
@@ -1,4 +1,58 @@
-// This is an input class. Do not edit.
+
+// First pointer travels a distance X
+// Second pointer travels a distance of 2x
+
+// F => X 
+// S => 2X 
+// For every one move the first pointer was doing
+// the second pointer was moving double the amount of the first pointer
+
+// Let's call the distance between our starting node
+// to the origin of the loop = D
+
+// Distance between the origin of the loop and 
+// the node where our pointers overlap = P
+
+
+// F => X => D + P
+// S => 2X => 2D + 2P
+
+// We want to find the distance between where the two pointers overlap
+// and the start of the loop as we go forward = R
+
+// So we have L
+
+// F = First Pointer
+// S = Second Pointer
+// D = Distance from first node to start of loop
+// P = Distance from start of loop to where the pointers overlap
+// R = Remainder of distance from the overlap point and the start of loop if we go forward
+
+// Goal is to move to return the node that starts the loop in the singly linked list
+
+// R = (D + P + R) - (D + P)
+
+
+
+// Our second pointer travels D, then P, then R + P
+// Travels the distance of entire linked list plus P
+
+// T = Distance of entire linked list
+
+// T = D + P + R
+// S  has travelled = D + P + R + P
+// S = 2D + 2P
+// Extra arc = P
+
+// T = 2D + 2P - P
+// P = 2D + P
+
+// R = T - P - D
+// R = 2D + P - P - D = D 
+
+
+
+
 class LinkedList {
     constructor(value) {
       this.value = value;
@@ -6,26 +60,25 @@ class LinkedList {
     }
   }
   
-  
-  // O(n) time | O(1) space
   function findLoop(head) {
-      // create two pointers
-   let first = head.next;
-      let second = first.next;
-      // our first node travels from the first nodes to the start of the loop
-      // all the way to the point it overlapped with the second pointer
-      // then traveled back from the head of the linked list
-      // all the way to the origin of the loop
-      // equal to the remainder of the linked list
+     //initialize pointers
+      let first = head.next;
+      let second = head.next.next;
       
-      // the second pointer did a bunch more
-      // in the same amount of time
+      // loop at diffrent speeds through the array 
+      // until they land on the same node
       while (first !== second){
           first = first.next;
           second = second.next.next;
       }
+      
+      // since
+      // we know the distance from the root node to the loop node
+      // is equal to the distance from the overlap node to the loop node
+      // we loop the first pointer and the second pointer at the same rate
+      // and we find where they overlap to find the loop node
       first = head;
-      while (first !== second) {
+      while (first !== second){
           first = first.next;
           second = second.next;
       }
@@ -35,4 +88,3 @@ class LinkedList {
   // Do not edit the lines below.
   exports.LinkedList = LinkedList;
   exports.findLoop = findLoop;
-  
