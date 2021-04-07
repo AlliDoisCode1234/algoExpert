b1154fe05659082db6ed289cacf6a232b7486c4d
James Fowler <josiah.james.fowler@gmail.com>
2021-04-07 13:21:11 -0400

    classPhotos

---
+++ 1Easy/Greedy-Algorithms/classPhotos.js
@@ -0,0 +1,21 @@
+// O(nlog(n)) time | O(1) space - where n is the number of students
+
+const classPhotos = (redShirtHeights, blueShirtHeights) => {
+    redShirtHeights.sort((a, b) => b - a);
+      blueShirtHeights.sort((a, b) => b - a);
+      
+      const shirtColorInFirstRow = redShirtHeights[0] < blueShirtHeights[0] ? 'RED' : 'BLUE';
+      for (let idx = 0; idx < redShirtHeights.length; idx++) {
+          const redShirtHeight = redShirtHeights[idx];
+          const blueShirtHeight = blueShirtHeights[idx];
+          
+          if (shirtColorInFirstRow === 'RED'){
+              if (redShirtHeight >= blueShirtHeight) return false;
+          } else if (blueShirtHeight >= redShirtHeight) return false;
+      }
+      
+      return true;
+  }
+  
+  // Do not edit the line below.
+  exports.classPhotos = classPhotos;
