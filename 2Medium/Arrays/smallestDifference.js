ca0a65f12a48a1ad18240ef7429758611cfc4a61
Josiah <josiah.james.fowler@gmail.com>
2021-08-02 10:59:27 -0400

    mergesort

--- 2Medium/Arrays/smallestDifference.js
+++ 2Medium/Arrays/smallestDifference.js
@@ -1,3 +1,5 @@
+// O(nlog(n) + mlog(m)) time | O(1) space
+
 function smallestDifference(arrayOne, arrayTwo) {
     arrayOne.sort((a,b) => a - b);
     arrayTwo.sort((a,b) => a - b);
--- 4Very-Hard/mergeSort.js
+++ 4Very-Hard/mergeSort.js
@@ -43,25 +43,67 @@
 //   SOLUTION #2 
 
 
-function merge (left, right) {
-	let arr = [];
+// function merge (left, right) {
+// 	let arr = [];
 	
-	while (left.length && right.length){
-		left[0] < right[0] ? arr.push(left.shift()) : arr.push(right.shift())
-	}
+// 	while (left.length && right.length){
+// 		left[0] < right[0] ? arr.push(left.shift()) : arr.push(right.shift())
+// 	}
 	
-	return [...arr, ...left, ...right]
-}
+// 	return [...arr, ...left, ...right]
+// }
 
+
+// function mergeSort(array) {
+//   const half = array.length / 2
+	
+	
+// 	if (array.length < 2) {
+// 		return array;
+// 	}
+	
+// 	const left = array.splice(0, half)
+// 	return merge(mergeSort(left),mergeSort(array))
+// }
+
+
+
+// SOLUTION #3 
+
+// Best: O(nlog(n)) time | O(nlog(n)) space
+// Average: O(nlog(n)) time | O(nlog(n)) space
+// Worst: O(nlog(n)) time | O(nlog(n)) space
+
+// [8, 5, 2, 9, 5, 6, 3]
 
 function mergeSort(array) {
-  const half = array.length / 2
-	
-	
-	if (array.length < 2) {
-		return array;
-	}
-	
-	const left = array.splice(0, half)
-	return merge(mergeSort(left),mergeSort(array))
-}
+	if (array.length <= 1) return array;
+	  const middleIdx = Math.floor(array.length / 2);
+	  const leftHalf = array.slice(0, middleIdx);
+	  const rightHalf = array.slice(middleIdx);
+	  return mergeSortedArrays(mergeSort(leftHalf), mergeSort(rightHalf));
+  }
+  
+  function mergeSortedArrays(leftHalf, rightHalf) {
+	  
+	  const sortedArray = new Array(leftHalf.length + rightHalf.length);
+	  
+	  let k = 0,
+			  i = 0,
+			  j = 0;
+	  while (i < leftHalf.length && j < rightHalf.length) {
+		  if (leftHalf[i] <= rightHalf[j]) {
+			  sortedArray[k++] = leftHalf[i++];
+		  } else {
+			  sortedArray[k++] = rightHalf[j++];
+		  }
+	  }
+	  while (i < leftHalf.length) {
+		  sortedArray[k++] = leftHalf[i++];
+	  }
+	  while (j < rightHalf.length) {
+		  sortedArray[k++] = rightHalf[j++]
+	  }
+	  return sortedArray
+  }
+  
