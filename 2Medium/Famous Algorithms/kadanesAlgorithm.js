6571e3a81542d7431b47cfa683b8d2cf5b0fff52
Josiah <josiah.james.fowler@gmail.com>
2021-08-02 12:12:30 -0400

    kadanes algorithm practice

--- 2Medium/Famous Algorithms/kadanesAlgorithm.js
+++ 2Medium/Famous Algorithms/kadanesAlgorithm.js
@@ -1,18 +1,30 @@
-// O(n) time | O(1) space - where n is the length of the input array
-const kadanesAlgorithm = array => {
-    // either maxSum = current number plus next number
-      // or just current number
-      // this accounts for negative numbers
+// // O(n) time | O(1) space - where n is the length of the input array
+// const kadanesAlgorithm = array => {
+//     // either maxSum = current number plus next number
+//       // or just current number
+//       // this accounts for negative numbers
       
-      // maxSumOf Ending Here = max(maxEndingHere + number)
-      // or
-      // just our number
-      let maxEndingHere = array[0];
-      let maxSoFar = array[0];
-      for (let i = 1; i < array.length; i++) {
-          const num = array[i];
-          maxEndingHere = Math.max(num, maxEndingHere + num);
-          maxSoFar = Math.max(maxSoFar, maxEndingHere)
-      }
-      return maxSoFar
-  }
+//       // maxSumOf Ending Here = max(maxEndingHere + number)
+//       // or
+//       // just our number
+//       let maxEndingHere = array[0];
+//       let maxSoFar = array[0];
+//       for (let i = 1; i < array.length; i++) {
+//           const num = array[i];
+//           maxEndingHere = Math.max(num, maxEndingHere + num);
+//           maxSoFar = Math.max(maxSoFar, maxEndingHere)
+//       }
+//       return maxSoFar
+//   }
+
+function kadanesAlgorithm(array) {
+	let maxEndingHere = array[0];
+	let maxSoFar = array[0];
+	for (let i = 1; i < array.length; i++) {
+		const  num = array[i];
+		maxEndingHere = Math.max(num, maxEndingHere + num)
+		maxSoFar = Math.max(maxSoFar, maxEndingHere);
+	}
+	
+	return maxSoFar;	
+}
